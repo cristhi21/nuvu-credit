6fd1a0794467f0b26ff9663d4800bebfd62a08e9
cvargas <cristhian.avc21@gmail.com>
2021-05-13 07:29:17 -0500

    commit init

---
+++ src/main/java/com/nuvucreditcvargas/nuvucredit/NuvuCreditApplication.java
@@ -0,0 +1,13 @@
+package com.nuvucreditcvargas.nuvucredit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuvuCreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuvuCreditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nuvucreditcvargas/nuvucredit/NuvuCreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuvucreditcvargas.nuvucredit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuvuCreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
