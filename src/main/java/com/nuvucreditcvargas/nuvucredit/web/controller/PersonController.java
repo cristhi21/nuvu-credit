a0024eb509376ccb6505c377ca14fefaa7a3a609
cvargas <cristhian.avc21@gmail.com>
2021-05-14 05:36:17 -0500

    Ajustes al controller Person

--- src/main/java/com/nuvucreditcvargas/nuvucredit/web/controller/PersonController.java
+++ src/main/java/com/nuvucreditcvargas/nuvucredit/web/controller/PersonController.java
@@ -3,11 +3,9 @@ package com.nuvucreditcvargas.nuvucredit.web.controller;
 import com.nuvucreditcvargas.nuvucredit.persistence.crud.PersonCrudRepository;
 import com.nuvucreditcvargas.nuvucredit.persistence.entity.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -28,7 +26,35 @@ public class PersonController {
     @RequestMapping(value="{id}")
     public ResponseEntity<Person> getPerson(@PathVariable("id") Long id){
         Optional<Person> optionalPerson = personCrudRepository.findById(id);
-        //return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         return optionalPerson.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.noContent().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Person> save(@RequestBody Person person){
+        return new ResponseEntity<>(personCrudRepository.save(person), HttpStatus.CREATED) ;
+    }
+
+    @PutMapping(value="{id}")
+    public ResponseEntity<Person> updatePerson(@PathVariable("id") final Long id, @RequestBody Person person){
+        Optional<Person> optionalPerson = personCrudRepository.findById(id);
+        System.out.println(optionalPerson);
+        if(optionalPerson.isPresent()) {
+            Person updatePerson = optionalPerson.get();
+            updatePerson.setNamePerson(person.getNamePerson());
+            updatePerson.setLastName(person.getLastName());
+            updatePerson.setCellphone(person.getCellphone());
+            updatePerson.setAdress(person.getAdress());
+            updatePerson.setMail(person.getMail());
+            personCrudRepository.save(updatePerson);
+            return ResponseEntity.ok(updatePerson);
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping(value="{id}")
+    public ResponseEntity<Void> deletePerson(@PathVariable("id") Long id){
+        personCrudRepository.deleteById(id);
+        return ResponseEntity.ok(null);
     }
 }
