618eedec3333ab1af209e5a83ddb035450839853
cvargas <cristhian.avc21@gmail.com>
2021-05-15 18:47:30 -0500

    Ajustes a los controllers:
    - CreditCard
    - Franchise

--- src/main/java/com/nuvucreditcvargas/nuvucredit/web/controller/CreditCardController.java
+++ src/main/java/com/nuvucreditcvargas/nuvucredit/web/controller/CreditCardController.java
@@ -2,13 +2,14 @@ package com.nuvucreditcvargas.nuvucredit.web.controller;
 
 import com.nuvucreditcvargas.nuvucredit.persistence.crud.CreditCardCrudRepository;
 import com.nuvucreditcvargas.nuvucredit.persistence.entity.CreditCard;
+import com.nuvucreditcvargas.nuvucredit.persistence.entity.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/credit_card")
@@ -18,8 +19,25 @@ public class CreditCardController {
     private CreditCardCrudRepository creditCardCrudRepository;
 
     @GetMapping
-    public ResponseEntity<List<CreditCard>> getCreditCard(){
+    public ResponseEntity<List<CreditCard>> getAllCreditCard(){
         List<CreditCard> persons = (List<CreditCard>) creditCardCrudRepository.findAll();
         return ResponseEntity.ok(persons);
+    }
+
+    @GetMapping(value="{id}")
+    public ResponseEntity<CreditCard> getCreditCard(@PathVariable("id") Long id){
+        Optional<CreditCard> optionalCreditCard = creditCardCrudRepository.findById(id);
+        return optionalCreditCard.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.noContent().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<CreditCard> save(@RequestBody CreditCard creditCard){
+        return new ResponseEntity<>(creditCardCrudRepository.save(creditCard), HttpStatus.CREATED) ;
+    }
+
+    @DeleteMapping(value="{id}")
+    public ResponseEntity<Void> deleteCreditCard(@PathVariable("id") Long id){
+        creditCardCrudRepository.deleteById(id);
+        return ResponseEntity.ok(null);
     }
 }
--- src/main/java/com/nuvucreditcvargas/nuvucredit/web/controller/FranchiseController.java
+++ src/main/java/com/nuvucreditcvargas/nuvucredit/web/controller/FranchiseController.java
@@ -1,14 +1,15 @@
 package com.nuvucreditcvargas.nuvucredit.web.controller;
 
 import com.nuvucreditcvargas.nuvucredit.persistence.crud.FranchiseCrudRepository;
+import com.nuvucreditcvargas.nuvucredit.persistence.entity.CreditCard;
 import com.nuvucreditcvargas.nuvucredit.persistence.entity.Franchise;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/franchise")
@@ -21,5 +22,22 @@ public class FranchiseController {
     public ResponseEntity<List<Franchise>> getAll(){
         List<Franchise> persons = (List<Franchise>) franchiseCrudRepository.findAll();
         return ResponseEntity.ok(persons);
+    }
+
+    @GetMapping(value="{id}")
+    public ResponseEntity<Franchise> getFranchise(@PathVariable("id") Integer id){
+        Optional<Franchise> optionalCreditCard = franchiseCrudRepository.findById(id);
+        return optionalCreditCard.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.noContent().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Franchise> save(@RequestBody Franchise franchise){
+        return new ResponseEntity<>(franchiseCrudRepository.save(franchise), HttpStatus.CREATED) ;
+    }
+
+    @DeleteMapping(value="{id}")
+    public ResponseEntity<Void> deleteCreditCard(@PathVariable("id") Integer id){
+        franchiseCrudRepository.deleteById(id);
+        return ResponseEntity.ok(null);
     }
 }
