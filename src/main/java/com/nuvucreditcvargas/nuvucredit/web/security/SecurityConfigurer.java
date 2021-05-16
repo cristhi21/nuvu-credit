005ecbe81289b61f965c57f682de25964c6df2f9
cvargas <cristhian.avc21@gmail.com>
2021-05-16 07:50:47 -0500

    v2 spring security
    - codificador de contraseñas para no tratarla con ningun hash

--- src/main/java/com/nuvucreditcvargas/nuvucredit/web/security/SecurityConfigurer.java
+++ src/main/java/com/nuvucreditcvargas/nuvucredit/web/security/SecurityConfigurer.java
@@ -2,9 +2,12 @@ package com.nuvucreditcvargas.nuvucredit.web.security;
 
 import com.nuvucreditcvargas.nuvucredit.domain.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
@@ -14,5 +17,10 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(myUserDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
