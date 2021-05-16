891cccf6b6612c0385f94109ffdf9117520bb5a0
cvargas <cristhian.avc21@gmail.com>
2021-05-15 20:58:04 -0500

    v1 spring security

---
+++ src/main/java/com/nuvucreditcvargas/nuvucredit/web/security/SecurityConfigurer.java
@@ -0,0 +1,18 @@
+package com.nuvucreditcvargas.nuvucredit.web.security;
+
+import com.nuvucreditcvargas.nuvucredit.domain.service.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(myUserDetailsService);
+    }
+}
